dbbd24ed7ff97ee35ddb8bf247c0f7de06f43412
user name <wlwrl999@gmail.com>
2024-04-12 01:52:54 +0900

    shell script for testing rainbow

---
+++ sunrise_ve/Atari_Rainbow/scripts/rainbow_ensemble.sh
@@ -0,0 +1,5 @@
+games=("crazy_climber" "jamesbond" "road_runner" "frostbite" "kangaroo" "chopper_command")
+
+for game in "${games[@]}"; do
+    python main.py --game $game --seed 123 --target-update 2000 --T-max 500000 --learn-start 1600 --memory-capacity 500000 --replay-frequency 1 --multi-step 20 --architecture data-efficient --hidden-size 256 --learning-rate 0.0001 --evaluation-interval 1000 --id eff_rainbow
+done

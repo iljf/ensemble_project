d1e1585c6783a2d0d783ee429ffe349e6989b3b7
user name <wlwrl999@gmail.com>
2024-08-28 22:50:56 +0900

    scripts for block id testing

---
+++ Ensemble/Sunrise/scripts/run_sunrise_block.sh
@@ -0,0 +1,3 @@
+for block in 0 1 2 3; do
+    python sunrise_100k_blockwise.py --game $1 --num-ensemble 5 --seed 122 --target-update 2000 --T-max 500000 --learn-start 1600 --memory-capacity 500000 --replay-frequency 1 --multi-step 20 --architecture data-efficient --hidden-size 256 --learning-rate 0.0001 --evaluation-interval 1000 --id sunrise --beta-mean $2 --temperature $3 --ucb-infer $4
+done
